b20ed841c9a11f4c7c95dd2d98ba144056332939
Marc Bontje <marc@rednose.nl>
2014-11-03 16:07:37 +0100

    Image widget plugin stubs

---
+++ src/plugins/image/dialogs/image.js
@@ -0,0 +1,55 @@
+﻿CKEDITOR.dialog.add( 'image', function( editor ) {
+    return {
+        title: 'Edit Simple Box',
+        minWidth: 200,
+        minHeight: 100,
+        contents: [
+            {
+                id: 'info',
+                elements: [
+                    {
+                        id: 'url',
+                        type: 'text',
+                        label: 'URL',
+                        //width: '50px',
+                        setup: function( widget ) {
+                            this.setValue( widget.data.url );
+                        },
+                        commit: function( widget ) {
+                            widget.setData( 'url', this.getValue() );
+                        }
+                    },
+                    {
+                        id: 'align',
+                        type: 'select',
+                        label: 'Align',
+                        items: [
+                            [ editor.lang.common.notSet, '' ],
+                            [ editor.lang.common.alignLeft, 'left' ],
+                            [ editor.lang.common.alignRight, 'right' ],
+                            [ editor.lang.common.alignCenter, 'center' ]
+                        ],
+                        setup: function( widget ) {
+                            this.setValue( widget.data.align );
+                        },
+                        commit: function( widget ) {
+                            widget.setData( 'align', this.getValue() );
+                        }
+                    },
+                    {
+                        id: 'width',
+                        type: 'text',
+                        label: 'Width',
+                        width: '50px',
+                        setup: function( widget ) {
+                            this.setValue( widget.data.width );
+                        },
+                        commit: function( widget ) {
+                            widget.setData( 'width', this.getValue() );
+                        }
+                    }
+                ]
+            }
+        ]
+    };
+} );
---
+++ src/plugins/image/plugin.js
@@ -0,0 +1,57 @@
+﻿CKEDITOR.plugins.add( 'image', {
+    requires: 'widget',
+
+    icons: 'image',
+
+    init: function( editor ) {
+        CKEDITOR.dialog.add( 'image', this.path + 'dialogs/image.js' );
+
+        editor.widgets.add( 'image', {
+
+            button: 'Create a simple box',
+
+            template:
+                '<figure>' +
+                    '<img src="http://verantwoordingsonderzoek.rekenkamer.nl/sites/default/files/images/2012-ienm-bi-begroting-grafiek.png">' +
+                '</figure>',
+
+            allowedContent:
+                'figure;' +
+                'img[!src];',
+
+            requiredContent: 'figure',
+
+            dialog: 'image',
+
+            upcast: function( element ) {
+                return element.name == 'figure';
+            },
+
+            init: function() {
+                var width = this.element.getStyle( 'width' );
+                if ( width )
+                    this.setData( 'width', width );
+                if ( this.element.hasClass( 'align-left' ) )
+                    this.setData( 'align', 'left' );
+                if ( this.element.hasClass( 'align-right' ) )
+                    this.setData( 'align', 'right' );
+                if ( this.element.hasClass( 'align-center' ) )
+                    this.setData( 'align', 'center' );
+            },
+
+            data: function() {
+
+                if ( this.data.width == '' )
+                    this.element.removeStyle( 'width' );
+                else
+                    this.element.setStyle( 'width', this.data.width );
+
+                this.element.removeClass( 'align-left' );
+                this.element.removeClass( 'align-right' );
+                this.element.removeClass( 'align-center' );
+                if ( this.data.align )
+                    this.element.addClass( 'align-' + this.data.align );
+            }
+        } );
+    }
+} );

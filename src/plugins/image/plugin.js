ee2310dac066b398ddb223f80a65778f59b4fdd4
Marc Bontje <marc@rednose.nl>
2014-11-07 15:29:32 +0100

    Image plugin implementation

--- src/plugins/image/plugin.js
+++ src/plugins/image/plugin.js
@@ -3,12 +3,44 @@
 
     icons: 'image',
 
+    afterInit: function (editor) {
+        editor.ui.add( 'Image', CKEDITOR.UI_BUTTON, {
+            label: 'Afbeelding invoegen',
+            command: 'image',
+            modes: {wysiwyg: 1},
+            editorFocus: 0,
+            toolbar: 'insert,30',
+            click: function () {
+                var form = document.getElementById('upload');
+                form.reset();
+
+                var attachment = document.getElementById('test');
+                attachment.click();
+
+                attachment.onchange = function () {
+                    var file = attachment.files[0], reader = new FileReader();
+
+                    if (!file) {
+                        return;
+                    }
+
+                    reader.onload = function () {
+                        editor.insertHtml('<figure><img src ="' + reader.result + '"/></figure>');
+                    };
+
+                    reader.readAsDataURL(file);
+                };
+            }
+        });
+    },
+
     init: function( editor ) {
         CKEDITOR.dialog.add( 'image', this.path + 'dialogs/image.js' );
 
         editor.widgets.add( 'image', {
-
-            button: 'Insert an image',
+            exec: function () {
+                console.log(arguments);
+            },
 
             template:
                 '<figure>' +
@@ -34,6 +66,11 @@
                 if (url) {
                     this.setData('url', url);
                 }
+
+                this.on( 'contextMenu', function( evt ) {
+                    evt.data.image = CKEDITOR.TRISTATE_OFF;
+                } );
+
             },
 
             data: function() {
@@ -43,6 +80,23 @@
                     img.setAttribute('src', this.data.url);
                 }
             }
+        } );
+
+        // Register context menu option for editing widget.
+        if ( editor.contextMenu ) {
+            editor.addMenuGroup( 'image', 10 );
+
+            editor.addMenuItem( 'image', {
+                label: 'Eigenschappen afbeelding',
+                command: 'image',
+                group: 'image'
+            } );
+        }
+
+        editor.ui.addButton && editor.ui.addButton( 'Image', {
+            label: 'Afbeelding invoegen',
+            command: 'image',
+            toolbar: 'insert,10'
         } );
     }
 } );

96ca9db550b74070f467f0fb2d6f34bd4638e611
Marc Bontje <marc@rednose.nl>
2014-11-07 11:28:41 +0100

    Image dialog

--- src/plugins/image/dialogs/image.js
+++ src/plugins/image/dialogs/image.js
@@ -1,13 +1,15 @@
 ﻿CKEDITOR.dialog.add( 'image', function( editor ) {
-    var urlField;
+    var urlData;
 
     return {
-        //onLoad: function () {
-        //    var dialog = CKEDITOR.dialog.getCurrent();
-        //    dialog.disableButton('ok');
-        //},
+        onLoad: function () {
+            // Disable button on load
+            var dialog = CKEDITOR.dialog.getCurrent();
+            dialog.disableButton('ok');
+            dialog.getButton('ok').getElement().addClass('disabled');
+        },
 
-        title: 'Edit Image',
+        title: 'Upload Image',
         minWidth: 310,
         minHeight: 100,
         contents: [
@@ -15,36 +17,39 @@
                 id: 'info',
                 elements: [
                     {
-                        id: 'url',
-                        type: 'text',
-                        label: 'URL',
-                        //width: '50px',
-                        onLoad: function () {
-                            urlField = this;
-                        },
-                        commit: function( widget ) {
-                            widget.setData( 'url', this.getValue() );
-                        }
-                    },
-                    {
                         type: 'html',
                         label: 'Upload',
-                        html: '<input type="file" name="attachment" id="attachment"/>',
+                        html: '<form id="upload"><input type="file" name="attachment" id="attachment"/></form>',
                         onLoad: function () {
                             var attachment = document.getElementById('attachment');
 
                             attachment.onchange = function () {
                                 var file = attachment.files[0], reader = new FileReader();
 
-                                reader.onload = function () {
-                                    //var dialog = CKEDITOR.dialog.getCurrent();
-                                    //dialog.enableButton('ok');
+                                if (!file) {
+                                    return;
+                                }
 
-                                    urlField.setValue(reader.result);
+                                reader.onload = function () {
+                                    var dialog = CKEDITOR.dialog.getCurrent();
+                                    dialog.enableButton('ok');
+                                    dialog.getButton('ok').getElement().removeClass('disabled');
+
+                                    urlData = reader.result;
                                 };
 
                                 reader.readAsDataURL(file);
                             };
+                        },
+                        onShow: function () {
+                            var form = document.getElementById('upload');
+                            form.reset();
+                        },
+                        setup: function( widget ) {
+                            urlData = widget.data.url;
+                        },
+                        commit: function( widget ) {
+                            widget.setData('url', urlData);
                         }
                    }
                 ]

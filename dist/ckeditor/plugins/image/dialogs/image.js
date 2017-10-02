ee233f3ed75614f9b2b8f84ee169ec4944bbb1bb
Marc Bontje <marc@rednose.nl>
2017-10-02 16:22:05 +0200

    Updated build

--- dist/ckeditor/plugins/image/dialogs/image.js
+++ dist/ckeditor/plugins/image/dialogs/image.js
@@ -1,1 +1,1 @@
-﻿CKEDITOR.dialog.add("image",function(){return{title:"Eigenschappen afbeelding",minWidth:310,minHeight:100,contents:[{id:"info",elements:[]}]}});
+﻿CKEDITOR.dialog.add("image",function(){return{title:"Image properties",minWidth:310,minHeight:100,contents:[{id:"info",elements:[{id:"alt_text",type:"text",label:"Alt text","default":"",setup:function(a){this.setValue(a.data.alt)},commit:function(a){a.setData("alt",this.getValue())}}]}]}});
